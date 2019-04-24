5c1a16d7f8b7992adb72305a735564ad58945b63
matbbastos <matbbastos@gmail.com>
2019-04-23 23:30:26 -0300

    Formatação do arquivo de Algoritmos

--- Algoritmos.py
+++ Algoritmos.py
@@ -1,3 +1,41 @@
+def buscaEmLargura(grafo, verticeInicial):
+    qtdVertices = grafo.qtdVertices()
+    foiVisitado = [False] * (qtdVertices + 1)  # lista de status (foi visitado?) de cada vértice
+    distanciaDeS = [999999999] * (qtdVertices + 1)  # lista de distâncias de s a cada vértice
+    antecessores = [None] * (qtdVertices + 1)  # lista de antecessores de cada vértice
+
+    foiVisitado[verticeInicial] = True  # vértice inicial inicializa visitado
+    distanciaDeS[verticeInicial] = 0  # distância até s = 0
+    filaIterativa = [verticeInicial]  # enfila vértice s
+
+    listaNiveis = {0: [verticeInicial]}
+
+    while len(filaIterativa) != 0:
+        verticeAtual = filaIterativa.pop(0)
+        listaVizinhos = grafo.vizinhos(verticeAtual)
+        for vizinho in listaVizinhos:
+            if not foiVisitado[vizinho]:
+                foiVisitado[vizinho] = True
+                distanciaDeS[vizinho] = distanciaDeS[verticeAtual] + 1
+                antecessores[vizinho] = verticeAtual
+                filaIterativa.append(vizinho)
+
+                vizinhosDoNivel = listaNiveis.get(distanciaDeS[vizinho], [])
+                vizinhosDoNivel.append(vizinho)
+                listaNiveis.update({distanciaDeS[vizinho]: vizinhosDoNivel})
+
+    printBuscaEmLargura(listaNiveis)
+
+
+    # for nivel in listaNiveis:
+    #     print(listaNiveis.get(nivel))
+
+    return (distanciaDeS, antecessores)
+
+def printBuscaEmLargura(listaNiveis):
+    for key, value in listaNiveis.items():
+        print(str(key) + ":", str(value)[1:-1])
+
 def algoritmoDeHierholzer(grafo):
     qtdDeArestas = grafo.qtdArestas()
     arestasVisitadas = [False] * qtdDeArestas
@@ -11,15 +49,6 @@ def algoritmoDeHierholzer(grafo):
         existe, resultado = True, ciclo
     printaCiclo(existe, resultado)
     return existe, resultado
-
-
-def printaCiclo(existe, resultado):
-    if existe:
-        print(1)
-        print(str(resultado)[1:-1])
-    else:
-        print(0)
-
 
 def buscaSubcicloEuleriano(grafo, vertice, foiVisitada):
     ciclo = [vertice]
@@ -73,53 +102,12 @@ def buscaSubcicloEuleriano(grafo, vertice, foiVisitada):
 
     return True, ciclo
 
-
-def listaTemItemNaOutraLista(lista1, lista2):
-    for item in lista1:
-        if item in lista2:
-            return True
-    return False
-
-
-def buscaEmLargura(grafo, verticeInicial):
-    qtdVertices = grafo.qtdVertices()
-    foiVisitado = [False] * (qtdVertices + 1)  # lista de status (foi visitado?) de cada vértice
-    distanciaDeS = [999999999] * (qtdVertices + 1)  # lista de distâncias de s a cada vértice
-    antecessores = [None] * (qtdVertices + 1)  # lista de antecessores de cada vértice
-
-    foiVisitado[verticeInicial] = True  # vértice inicial inicializa visitado
-    distanciaDeS[verticeInicial] = 0  # distância até s = 0
-    filaIterativa = [verticeInicial]  # enfila vértice s
-
-    listaNiveis = {0: [verticeInicial]}
-
-    while len(filaIterativa) != 0:
-        verticeAtual = filaIterativa.pop(0)
-        listaVizinhos = grafo.vizinhos(verticeAtual)
-        for vizinho in listaVizinhos:
-            if not foiVisitado[vizinho]:
-                foiVisitado[vizinho] = True
-                distanciaDeS[vizinho] = distanciaDeS[verticeAtual] + 1
-                antecessores[vizinho] = verticeAtual
-                filaIterativa.append(vizinho)
-
-                vizinhosDoNivel = listaNiveis.get(distanciaDeS[vizinho], [])
-                vizinhosDoNivel.append(vizinho)
-                listaNiveis.update({distanciaDeS[vizinho]: vizinhosDoNivel})
-
-    printBuscaEmLargura(listaNiveis)
-
-
-    # for nivel in listaNiveis:
-    #     print(listaNiveis.get(nivel))
-
-    return (distanciaDeS, antecessores)
-
-
-def printBuscaEmLargura(listaNiveis):
-    for key, value in listaNiveis.items():
-        print(str(key) + ":", str(value)[1:-1])
-
+def printaCiclo(existe, resultado):
+    if existe:
+        print(1)
+        print(str(resultado)[1:-1])
+    else:
+        print(0)
 
 def dijkstra(grafo, verticeInicial):
     qtdVertices = grafo.qtdVertices()
@@ -162,8 +150,13 @@ def dijkstra(grafo, verticeInicial):
     printDijkstra(distanciaDoInicial, antecessores)
     return distanciaDoInicial, antecessores
 
-
 def printDijkstra(distancias, antecessores):
     for index in range(1,len(antecessores)):
         array = str(antecessores[index])[1:-1]
         print(str(index)+":",array+"; d="+str(distancias[index]))
+
+def listaTemItemNaOutraLista(lista1, lista2):
+    for item in lista1:
+        if item in lista2:
+            return True
+    return False
