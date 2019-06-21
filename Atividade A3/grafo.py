9e22c827ce25d2723023c16dfe970231ae36ec67
matbbastos <matbbastos@gmail.com>
2019-06-21 16:01:06 -0300

    Adição da Atividade A3

---
+++ Atividade A3/grafo.py
@@ -0,0 +1,108 @@
+import copy
+
+
+class Grafo:
+    # vertices = { v1: {rotulo: "nome", v2:6,indexDasArestas:[5,6]}, v2: {rotulo: "nome", v3:5, v1:6}}
+    # arestas [ (v1, v2, 6) (v2, v3, 2)]
+
+    def __init__(self, nomeDoArquivo=False, vertices={}, arestas=[]):
+        self.vertices = vertices
+        self.arestas = arestas
+        if nomeDoArquivo:
+            self.leArquivo(nomeDoArquivo)
+
+    def getArestas(self):
+        return self.arestas
+
+    def getVertices(self):
+        return self.vertices.keys()
+
+    def qtdVertices(self):
+        return len(self.vertices.keys())
+
+    def qtdArestas(self):
+        return len(self.arestas)
+
+    def grau(self, vertice):
+        return (len(self.vertices.get(vertice)) - 2)
+
+    def getIndexDasArestasDoVertice(self, vertice):
+        return self.vertices.get(vertice).get("indexDasArestas")
+
+    def rotulo(self, vertice):
+        return self.vertices.get(vertice).get("rotulo")
+
+    def vizinhos(self, vertice):
+        dicionarioVizinhosDoVertice = copy.copy(self.vertices.get(vertice))
+        dicionarioVizinhosDoVertice.pop("rotulo")
+        dicionarioVizinhosDoVertice.pop("indexDasArestas")
+        return dicionarioVizinhosDoVertice
+
+    def haAresta(self, vertice1, vertice2):
+        return vertice2 in self.vertices.get(vertice1)
+
+    def peso(self, vertice1, vertice2):
+        return self.vertices.get(vertice1).get(vertice2, 999999999)
+
+    def leArquivo(self, nomeArquivo):
+        arquivo = open(nomeArquivo, "r")
+
+        taNaParteDeEdges = False
+
+        for linha in arquivo:
+            if "vertice" in linha:
+                continue
+
+            if "edge" in linha or "arc" in linha:
+                taNaParteDeEdges = linha
+                continue
+
+            if not taNaParteDeEdges:
+                vertice, rotulo = linha.split()
+                vertice = int(vertice)
+                self.vertices.update(
+                    {vertice: {"rotulo": rotulo, "indexDasArestas": []}})
+
+            if taNaParteDeEdges:
+                vertice, vizinho, peso = linha.split()
+                vertice = int(vertice)
+                vizinho = int(vizinho)
+                peso = float(peso)
+
+                size = len(self.arestas)
+                if "edge" in taNaParteDeEdges:
+                    indexesVizinho = self.vertices.get(
+                        vizinho).get("indexDasArestas")
+                    indexesVizinho.append(size)
+                    self.vertices.get(
+                        vizinho).update({vertice: peso,
+                                        "indexDasArestas": indexesVizinho})
+
+                indexesVertice = self.vertices.get(
+                    vertice).get("indexDasArestas")
+                indexesVertice.append(size)
+                self.vertices.get(
+                    vertice).update({vizinho: peso,
+                                    "indexDasArestas": indexesVertice})
+
+                self.arestas.append((vertice, vizinho, peso))
+
+    def getGrafoTransposto(self):
+        novoDicVertices = {}
+        novaListaArestas = []
+        for vertice in self.vertices.keys():
+            novoDicVertices.update(
+                    {vertice: {
+                        "rotulo": self.vertices.get(vertice).get('rotulo'),
+                        "indexDasArestas": []}})
+
+        for vertice, vizinho, peso in self.arestas:
+            size = len(novaListaArestas)
+            indexesVizinho = novoDicVertices.get(
+                            vizinho).get("indexDasArestas")
+            indexesVizinho.append(size)
+            novoDicVertices.get(
+                vizinho).update({vertice: peso,
+                                "indexDasArestas": indexesVizinho})
+            novaListaArestas.append((vizinho, vertice, peso))
+        return Grafo(vertices=novoDicVertices, arestas=novaListaArestas)
---
+++ Atividade A3/main.py
@@ -0,0 +1,42 @@
+"""
+Usage: main.py (<arquivoDeGrafo>) [options]
+          main.py -h | --help
+
+Options:
+  -h --help     se não especificado, chamam-se todas as funções
+  -c            executa algoritmo de componentes fortemente conexas
+  -o            executa algoritmo de ordenação topológica
+  -a            executa algoritmo de Prim para árvore geradora mínima
+"""
+import algoritmos
+from grafo import Grafo
+from grafoDirigido import GrafoDirigido
+from docopt import docopt
+
+
+def check_args(args):
+    if (not ['<arquivoDeGrafo>']):
+        exit("(<arquivoDeGrafo>) é necessário!\
+                \n Use '-h' para ajuda")
+    selecionaAlgoritmos(args)
+
+
+def selecionaAlgoritmos(args):
+    grafo = Grafo(args['<arquivoDeGrafo>'])
+    grafoDirigido = GrafoDirigido(args['<arquivoDeGrafo>'])
+    executaTodos = not(args['-c'] or args['-o'] or args['-a'])
+
+    if args['-c'] or executaTodos:
+        print("\nExecutando Algoritmo de Componentes Fortemente Conexas (Kosaraju)")
+        algoritmos.kosaraju(grafo)
+    if args['-o'] or executaTodos:
+        print("\nExecutando Algoritmo de Ordenação Topológica")
+        algoritmos.ordenacaoTopologica(grafoDirigido)
+    if args['-a'] or executaTodos:
+        print("\nExecutando Algoritmo de Árvore Geradora Mínima (Prim)")
+        algoritmos.prim(grafo)
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    check_args(args)
